b9c6bc9249ba16594aecc786dd052585418a4436
drmidknight <draco1gh@gmail.com>
2023-08-23 14:16:17 +0000

    array response

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import ListHeader from './components/ListHeader';
+import { useEffect } from 'react'
 
 const App = () => {
 
-  const getData = () => {
-    try {
+  const getData = async () => {
+    const userEmail = 'test@gmail.com'
 
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
     } catch (err) {
       console.error(err)
     }
   }
 
+  useEffect(() => getData, [])
 
   return (
     <div className="app">

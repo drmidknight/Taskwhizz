e5dfbcb1c7dea3fa478c16030924c2b37f33ff83
drmidknight <draco1gh@gmail.com>
2023-08-23 11:49:18 +0000

    Backend services working

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,18 @@ import ListHeader from './components/ListHeader';
 
 const App = () => {
 
+  const getData = () => {
+    try {
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
 
   return (
     <div className="app">
-     <ListHeader listName={'🌴Holiday tick list'}/>     
+     <ListHeader listName={'🌴 Holiday tick list'}/>     
     </div>
   )
 }
